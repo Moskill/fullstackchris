e15ea76f7b6c1f6d11beb748b54b77a5008ab133
unknown <cborek@web.de>
2022-05-15 22:26:12 +0200

    Den Header weiter gebaut

--- themes/Full-Stack-Theme/js/main.js
+++ themes/Full-Stack-Theme/js/main.js
@@ -18,4 +18,18 @@ function move() {
 }
 
 
+// scrolling navbar
+$(document).ready(function() {
+  $(window).scroll(function () {
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 280) {
+      $('.navbar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 281) {
+      $('.navbar').removeClass('navbar-fixed');
+    }
+  });
+});
+
+
 // Erstmal testen :)
